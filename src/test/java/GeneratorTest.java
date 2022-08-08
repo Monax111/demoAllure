4e424ec61f86b399c9d2c40c27c1afed42934510
tim <t.p.maerov@tinkoff.ru>
2022-08-08 14:42:49 +0300

    нет результата алюра после интеграционных тестов

--- src/test/java/GeneratorTest.java
+++ src/test/java/GeneratorTest.java
@@ -7,7 +7,7 @@ class GeneratorTest {
 
     @Test
     void countPositiveEqualsCountNegative() {
-        double result = new Generator().execute(1000);
+        double result = new Generator().execute(10000);
         System.out.println(result);
         Assertions.assertTrue(result > 0.9);
         Assertions.assertTrue(result < 1.1);
